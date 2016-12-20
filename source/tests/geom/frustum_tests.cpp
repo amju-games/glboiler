74a30901a99163682c02d7ce39988bb08640a926
Jason Colman <jason@touchsurgery.com>
2016-12-20 16:04:13 +0000

    Add stub for frustum tests

---
+++ source/tests/geom/frustum_tests.cpp
@@ -0,0 +1,12 @@
+// -----------------------------------------------------------------------------
+// glboiler - Jason Colman 2016 - OpenGL experiments
+// -----------------------------------------------------------------------------
+
+#include <gtest/gtest.h>
+#include "frustum.h"
+
+TEST(frustum, create)
+{
+    frustum f;
+}
+
