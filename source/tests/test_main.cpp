a5f6d50ed8de1b388549b3bc915766bd6b5b0dac
Jason Colman <jason@amju.com>
2016-12-30 18:19:52 +0000

    Create a GL context in test main: this is so we can run GL commands as part of the tests

--- source/tests/test_main.cpp
+++ source/tests/test_main.cpp
@@ -8,14 +8,21 @@
 //   cd source/thirdparty/googletest/make
 //   make gtest.a
 // Then back in source/tests
-//   clang++ *.cpp ../*.cpp -I .. -I ../thirdparty/googletest/include  -std=c++11  ../thirdparty/googletest/make/gtest.a
+//   make  
 
+#include "gl_includes.h"
 #include <gtest/gtest.h>
 
 int main(int argc, char * argv[])
 {
-  //setup tests
   ::testing::InitGoogleTest(&argc, argv);
+
+  // Set up GL context so we can do tests involving OpenGL
+  glutInit(&argc, argv);
+  glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGB);
+  glutInitWindowSize(100, 100);
+  glutCreateWindow("Hello tests");
+
   return RUN_ALL_TESTS();
 }
 
