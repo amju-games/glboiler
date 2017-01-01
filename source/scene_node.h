e9c497add36ff3724ffd1ddd644efa1d428925c3
Jason Colman <jason@amju.com>
2017-01-01 15:05:24 +0000

    Scene node xform + tests

--- source/scene_node.h
+++ source/scene_node.h
@@ -7,11 +7,20 @@
 #include <memory>
 #include "bounding_vol.h"
 #include "frustum.h"
+#include "mat4.h"
 #include "object.h"
 
 class scene_node : public object
 {
 public:
+  scene_node()
+  {
+    load_identity(m_xform);
+    m_has_changed = false;
+  }
+
+  bool has_changed() const { return m_has_changed; }
+
   virtual const bounding_vol& get_bounding_vol() const
   {
     return *m_bounding_vol;
@@ -19,8 +28,13 @@ public:
 
   virtual void render() const = 0;
 
+  const mat4& get_world_xform() const { return m_xform; }
+  mat4& get_world_xform() { m_has_changed = true; return m_xform; }
+
 protected:
+  mat4 m_xform;
   std::unique_ptr<bounding_vol> m_bounding_vol;
+  bool m_has_changed;
 };
 
 using p_scene_node = std::shared_ptr<scene_node>;
--- source/tests/scene_node_tests.cpp
+++ source/tests/scene_node_tests.cpp
@@ -5,10 +5,29 @@
 #include <gtest/gtest.h>
 #include "test_scene_node.h"
 #include "frustum.h"
+#include "mat4_test_utils.h"
 
 TEST(scene_node, render)
 {
-    test_scene_node n;
-    n.render();
+  test_scene_node n;
+  n.render();
 }
+
+TEST(scene_node, initial_xform)
+{
+  test_scene_node n;
+  assert_identity(n.get_world_xform());
+}
+
+TEST(scene_node, change_xform)
+{
+  test_scene_node n;
+  mat4 m2 = { 1, 2, 3, 4,  5, 6, 7, 8,  9, 10, 11, 12,  13, 14, 15, 16 };
+  mat4 result;
+  mult(n.get_world_xform(), m2, result);
+  assert_equal(result, m2);
+
+  ASSERT_TRUE(n.has_changed());
+}
+
 
