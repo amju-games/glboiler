95e5a492039ca4837fde5b339bb70f904dfa8fde
Jason Colman <jason@amju.com>
2017-01-02 20:49:54 +0000

    Temporary plane under sphere to show shadow better

--- source/teapot_scene_node.cpp
+++ source/teapot_scene_node.cpp
@@ -13,7 +13,17 @@ teapot_scene_node::teapot_scene_node()
 
 void teapot_scene_node::render() const
 {
-    glutSolidSphere(1, 100, 100);
+  glutSolidSphere(1, 100, 100);
 //    glutSolidTeapot(1);
+
+  glBegin(GL_QUADS);
+  float x = 5.0f;
+  float y = -2.0f;
+  float z = 5.0f;
+  glVertex3f(x, y, z);
+  glVertex3f(x, y, -z);
+  glVertex3f(-x, y, -z);
+  glVertex3f(-x, y, z);
+  glEnd();
 }
 
